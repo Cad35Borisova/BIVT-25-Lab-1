6c434f794df62f439fa554d9bc7516c1543e564b
Borisova Kira <bor1sova.kira@yandex.ru>
2025-10-01 22:37:10 +0300

    Borisova KA green l Lab 1

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,10 @@
             bool answer = false;
 
             // code here
-
+         if (Math.Abs(d) >= 1)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +20,11 @@
             bool answer = false;
 
             // code here
+            if ((d + f) / 2 > 0)
+            {
+                answer = true;
 
+            }
             // end
 
             return answer;
@@ -27,7 +34,11 @@
             bool answer = false;
 
             // code here
-
+             double s = a + b;
+             double m = (Math.Abs(a) + Math.Abs(b)) / 2;
+            if (s > m) {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -37,7 +48,18 @@
             int answer = 0;
 
             // code here
-
+            if (  a > c && a > b)
+            {
+                answer = a;
+            }
+            if (b > c && b > a)
+            {
+                answer = b;
+            }
+            if (c > b && c > a) 
+            {
+                answer = c;
+            }
             // end
 
             return answer;
@@ -47,7 +69,15 @@
             double answer = 0;
 
             // code here
-
+             double a = Math.Abs(x);
+            if (a > 1)
+            {
+                answer = 0;
+            }
+            if (a <= 1)
+            {
+                answer = x * x - 1;
+            }
             // end
 
             return answer;
@@ -57,6 +87,10 @@
             bool answer = false;
 
             // code here
+            if (y >= 0 && y <= x +1 && y <= -x +1)
+            {
+                answer = true;
+            }
 
             // end
 
@@ -68,17 +102,38 @@
             bool answer = true;
 
             // code here
+            if (n < 0)
+            {
+                answer = false;
+            }
+            else if (n % 2 == 0)
+            {
+                answer = false;
+            }
+          
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
 
             // code here
-
+            if (X < 7 ){
+                return false;
+            }
+            int nt = (X + 1) / 2;
+            int ts = nt * Y;
+            int tt = (4 * 60 - ts) % 1440;
+            if (tt < 0)
+            {
+                tt += 1440;
+            }
+            if (tt == 0 || tt >= 22 * 60)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
